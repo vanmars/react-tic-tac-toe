c12388a755cca77debf86ed1e6fd279200ffb364
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-01 14:26:03 -0800

    PASS test for history reducer with add board action type

--- src/reducers/history-reducer.js
+++ src/reducers/history-reducer.js
@@ -1,3 +1,12 @@
 export default (state = {}, action) => {
-  return state;
+  const { type, squares } = action;
+  switch(type) {
+    case 'ADD_BOARD':
+      const newObject = {
+        squares: squares
+      }
+      return Array(...state, newObject)
+    default:
+      return state;
+  };
 }
