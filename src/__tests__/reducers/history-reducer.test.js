937812676b06dc91cf5df599fd35cd43b2379707
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-01 14:17:44 -0800

    Write and FAIL test for adding a board of squares to history state

--- src/__tests__/reducers/history-reducer.test.js
+++ src/__tests__/reducers/history-reducer.test.js
@@ -1,8 +1,29 @@
 import historyReducer from '../../reducers/history-reducer';
 
 describe ('historyReducer', () => {
+
+  let action;
+  // squares: Array(9).fill(null),
+  const boardData = {
+    squares: [
+      null, null, null,
+      null, null, null,
+      null, null, null
+    ]
+  };
+  // Test for adding a board to the history
+  test('should successfully add a new board object to the history array', () => {
+    const { squares } = boardData; 
+    action = {
+      type: 'ADD_BOARD',
+      squares: squares
+    };
+    expect(historyReducer([], action)).toEqual([
+      { squares: [ null, null, null, null, null, null, null, null, null]}
+    ])
+  })
   // Test for no action
-test('shouyld return default state if there is no action type passed into the reducer', () => {
-    expect(historyReducer({}, {type:null})).toEqual({});
+test('should return default state if there is no action type passed into the reducer', () => {
+    expect(historyReducer([], {type:null})).toEqual([]);
   });
 })
