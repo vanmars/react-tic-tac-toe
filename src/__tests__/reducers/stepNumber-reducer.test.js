35b405e662c2dd82a143d14064938a069a9ae35e
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-02 11:23:59 -0800

    Write and FAIL test for step number reducer reset action
    Co-authored-by: Johnny Duverseau <duverseaujohnny21@gmail.com>
    Co-authored-by: Ophelia Swen <ophelia.swen@gmail.com>

--- src/__tests__/reducers/stepNumber-reducer.test.js
+++ src/__tests__/reducers/stepNumber-reducer.test.js
@@ -8,5 +8,12 @@ describe('stepNumberReducer', () => {
 test('should successfully add a step to the stepNumber state', () => {
     expect(stepNumberReducer(0, {type: 'ADD_STEP'})).toEqual(1)
   });
-  
+
+test('should successfully reset step to the stepNumber state', () => {
+  const action = {
+    type: 'RESET_STEP',
+    step: 0
+  }
+  expect(stepNumberReducer(9, action)).toEqual(0)
+  });
 })
--- src/reducers/stepNumber-reducer.js
+++ src/reducers/stepNumber-reducer.js
@@ -2,7 +2,9 @@ export default (state = 0, action) => {
   switch(action.type) {
     case 'ADD_STEP':
       return state + 1;
+    case 'RESET_STEP':
+      return !state;
     default:
       return state;
   }
-}
+};
