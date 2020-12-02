b85c82922957d2a5d5cc8a6354e86e6037b78280
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-02 11:04:44 -0800

    Write and FAIL test for step number reducer with null action type
    Co-authored-by: Johnny Duverseau <duverseaujohnny21@gmail.com>
    Co-authored-by: Ophelia Swen <ophelia.swen@gmail.com>

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import xIsNextReducer from './xIsNext-reducer';
 import historyReducer from './history-reducer';
+import stepNumberReducer from './stepNumber-reducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
   xIsNext: xIsNextReducer,
-  history: historyReducer
+  history: historyReducer,
+  stepNumber: stepNumberReducer
 });
 
 export default rootReducer;
