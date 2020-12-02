68ccad2c6b361ac609f32a7629398feeffa55fe1
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-02 14:39:25 -0800

    Add action constants file
    Co-authored-by: Johnny Duverseau <duverseaujohnny21@gmail.com>
    Co-authored-by: Ophelia Swen <ophelia.swen@gmail.com>

---
+++ src/actions/ActionTypes.js
@@ -0,0 +1,5 @@
+export const ADD_BOARD = 'ADD_BOARD';
+export const DELETE_BOARD = 'DELETE_BOARD';
+export const TOGGLE = 'TOGGLE';
+export const ADD_STEP= 'ADD_STEP';
+export const RESET_STEP = 'RESET_STEP';
