5d092bcd7ad19c770c152ccd63ce4370d49900b1
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-02 15:51:47 -0800

    Refactor by creating individual files for components and helper functions
    Co-authored-by: Johnny Duverseau <duverseaujohnny21@gmail.com>
    Co-authored-by: Ophelia Swen <ophelia.swen@gmail.com>

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Game from './Game';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Game />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Square.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Square (props) {
+  return (
+    <button 
+      className="square" 
+      onClick={props.onClick} >
+      {props.value}
+    </button>
+  );
+}
+
+Square.propTypes = {
+  value: PropTypes.string,
+  onClick: PropTypes.func
+}
+
+export default Square;
