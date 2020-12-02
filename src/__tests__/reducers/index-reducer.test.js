69d7e7956be73086de18f9946124fb66caa0ac52
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-02 09:58:22 -0800

    PASS test for returning initial state of history reducer  with no action type
    Co-authored-by: Johnny Duverseau <duverseaujohnny21@gmail.com>
    Co-authored-by: Ophelia Swen <ophelia.swen@gmail.com>

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,11 +1,18 @@
 import rootReducer from '../../reducers/index';
 import xIsNextReducer from '../../reducers/xIsNext-reducer';
+import { createStore }  from 'redux';
+import historyReducer from '../../reducers/history-reducer';
+
+let store = createStore(rootReducer)
 
 describe('rootReducer', () => {
-  test('should return default state if no action type given', () => {
+test('should return default state if no action type given', () => {
     expect(rootReducer({}, { type: null })).toEqual({
       xIsNext: true,
       history: [{ squares: [ null, null, null, null, null, null, null, null, null] }]
     })
+  })
+  test('Check that initial state of historyReducer matches root reducer', () => {
+    expect(store.getState().history).toEqual(historyReducer(undefined, {type: null}))
   })
 })
